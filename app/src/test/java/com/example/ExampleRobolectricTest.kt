299ae004813973bcf177086339e4488e6480a3e5
hemantk-cloud <hemant.k@allen.in>
2026-06-12 11:39:08 +0530

    chore: rename application to Hemant Gurjar HRMS
    Update the app display name and corresponding unit tests to reflect the rebranding.

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -16,6 +16,6 @@ class ExampleRobolectricTest {
   fun `read string from context`() {
     val context = ApplicationProvider.getApplicationContext<Context>()
     val appName = context.getString(R.string.app_name)
-    assertEquals("Zoho People HRMS", appName)
+    assertEquals("Hemant Gurjar HRMS", appName)
   }
 }
